5c0b0366486e7d68ec7dd94858ba3f3379a6c5df
UsmanCh1226 <usmanch@umich.edu>
2026-01-25 17:19:56 -0500

    Implementing random forest technique

--- src/random_forest_model.py
+++ src/random_forest_model.py
@@ -6,6 +6,7 @@ FEATURES_PATH = "../data/processed/spacex_features.csv"
 TARGET_PATH = "../data/processed/spacex_target.csv"
 
 def train_random_forest():
+    # Load data
     X = pd.read_csv(FEATURES_PATH)
     y = pd.read_csv(TARGET_PATH).squeeze()
 
@@ -14,6 +15,7 @@ def train_random_forest():
     X = X.loc[mask]
     y = y.loc[mask].astype(int)
 
+    # Split data
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
@@ -22,17 +24,20 @@ def train_random_forest():
         stratify=y
     )
 
+    # Train Random Forest
     rf = RandomForestClassifier(
-        n_estimators=200,
-        max_depth=6,
+        n_estimators=200,      # number of trees
+        max_depth=6,           # prevents overfitting
         class_weight="balanced",
         random_state=42
     )
 
     rf.fit(X_train, y_train)
 
-    print("Random Forest trained!")
+    print("Random Forest trained successfully!")
+
     return rf, X_test, y_test
+
 
 if __name__ == "__main__":
     train_random_forest()
